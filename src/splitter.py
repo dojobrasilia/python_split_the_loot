c6f200e9ee9558cb22507c4664a4d8c7885316a3
Fabricio Nogueira Buzeto <nuk.anime13@gmail.com>
2012-01-25 17:42:56 -0200

    Just and extrac method refactor

--- src/splitter.py
+++ src/splitter.py
@@ -1,17 +1,23 @@
 class Splitter(object):
     def split(self,loot,pirate_number):
         loot.sort()
+        ret = self.init_answer(pirate_number)
+
+        i =0
+        while i < loot.__len__():
+            ret[i%pirate_number].append(loot[i])
+            i += 1
+            
+        print loot
+        print ret
+        return ret
+    
+    def init_answer(self,pirate_number):
         ret = []
         i = 0
         #init answer
         while i < pirate_number:
             ret.append([])
             i += 1
-        #split the loot
-        i =0
-        while i < loot.__len__():
-            ret[i%pirate_number].append(loot[i])
-            i += 1
-        print loot
-        print ret
         return ret
+    
