d50459f4b048d77772a3c6ed23b620302ddf9cd4
Fabricio Nogueira Buzeto <nuk.anime13@gmail.com>
2012-01-25 17:30:48 -0200

    Now we solve the problem if all the entrys are equal =P

--- src/splitter.py
+++ src/splitter.py
@@ -1,6 +1,12 @@
 class Splitter(object):
     def split(self,loot,pirate_number):
         ret = []
-        ret.append([loot[0]])
-        ret.append([loot[1]])
+        i = 0
+        while i < pirate_number:
+            j = 0
+            ret.append([])
+            while j < (loot.__len__()/pirate_number):
+                ret[i].append(loot[i+j])
+                j += 1
+            i += 1
         return ret
--- src/test_splitter.py
+++ src/test_splitter.py
@@ -1,14 +1,15 @@
 from unittest import TestCase
 from splitter import Splitter
 class SplitterTest(TestCase):
-    
+    def setUp(self):
+        self.s = Splitter()
+
     def test_spliting_for_a_single_pirate_is_trivial(self):
-        s = Splitter()
-        self.assertEqual(s.split([2,2,3],1), [[2,2,3]], "Should be trivial")
-        self.assertEqual(s.split([1,3],1), [[1,3]], "Should be trivial")
+        self.assertEqual(self.s.split([2,2,3],1), [[2,2,3]])
+        self.assertEqual(self.s.split([1,3],1), [[1,3]])
         pass
     
     def test_spliting_an_homogeneous_array_leads_to_an_homogeneous_answer(self):
-        s = Splitter()
-        self.assertEqual(s.split([2,2],2), [[2],[2]], "Should be trivial")
+        self.assertEqual(self.s.split([2,2],2), [[2],[2]])
+        self.assertEqual(self.s.split([2,2,2,2,2,2],3), [[2,2],[2,2],[2,2]])
         pass
