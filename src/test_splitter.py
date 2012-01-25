c4a3d0e3ded18090aebbdadb751d50fe2152d4bb
Fabricio Nogueira Buzeto <nuk.anime13@gmail.com>
2012-01-25 17:44:57 -0200

    Refactoring the name of the tests

--- src/test_splitter.py
+++ src/test_splitter.py
@@ -4,17 +4,16 @@ class SplitterTest(TestCase):
     def setUp(self):
         self.s = Splitter()
 
-    def test_spliting_for_a_single_pirate_is_trivial(self):
+    def test_for_a_single_pirate_is_trivial(self):
         self.assertEqual(self.s.split([2,2,3],1), [[2,2,3]])
         self.assertEqual(self.s.split([1,3],1), [[1,3]])
-        pass
     
-    def test_spliting_an_homogeneous_loot_leads_to_an_homogeneous_answer(self):
+    def test_an_homogeneous_loot_leads_to_an_homogeneous_answer(self):
         self.assertEqual(self.s.split([2,2],2), [[2],[2]])
         self.assertEqual(self.s.split([2,2,2,2,2,2],3), [[2,2],[2,2],[2,2]])
-        pass
     
-    def test_spliting_a_loot_with_a_even_number_of_elements_an_single_kind_of_answer(self):
+    def test_a_loot_with_a_even_number_of_elements_an_single_kind_of_answer(self):
         self.assertEqual(self.s.split([1,2,1,2],2), [[1,2],[1,2]])
         self.assertEqual(self.s.split([1,1,2,2],2), [[1,2],[1,2]])
-        pass
+        self.assertEqual(self.s.split([1,3,1,2,3,3,2,1,2],3), [[1,2,3],[1,2,3],[1,2,3]])
+    
