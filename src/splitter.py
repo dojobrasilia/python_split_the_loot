0ece6d54b8644b1de5e5d3e9110e7e8cf9d1b5fa
Fabricio Nogueira Buzeto <nuk.anime13@gmail.com>
2012-01-25 17:59:46 -0200

    Decided to focus on the eager path to solve the problem

--- src/splitter.py
+++ src/splitter.py
@@ -1,13 +1,26 @@
 class Splitter(object):
     def split(self,loot,pirate_number):
         loot.sort()
+        
         ret = self.init_answer(pirate_number)
-
+        
+        bucket_size = 0
+        for l in loot:
+            bucket_size += l
+        bucket_size = bucket_size/pirate_number
+        
         i =0
+        pirate_index = 0
         while i < loot.__len__():
-            ret[i%pirate_number].append(loot[i])
-            i += 1
-            
+            pirate_bucket = ret[pirate_index]
+            sum = 0 
+            while sum < bucket_size:
+                pirate_bucket.append(loot[i])
+                sum += loot[i]
+                i += 1
+            pirate_index += 1
+                
+        print bucket_size
         print loot
         print ret
         return ret
--- src/test_splitter.py
+++ src/test_splitter.py
@@ -12,8 +12,11 @@ class SplitterTest(TestCase):
         self.assertEqual(self.s.split([2,2],2), [[2],[2]])
         self.assertEqual(self.s.split([2,2,2,2,2,2],3), [[2,2],[2,2],[2,2]])
     
-    def test_a_loot_with_a_even_number_of_elements_an_single_kind_of_answer(self):
+    def _test_a_loot_with_a_even_number_of_elements_an_single_kind_of_answer(self):
         self.assertEqual(self.s.split([1,2,1,2],2), [[1,2],[1,2]])
         self.assertEqual(self.s.split([1,1,2,2],2), [[1,2],[1,2]])
         self.assertEqual(self.s.split([1,3,1,2,3,3,2,1,2],3), [[1,2,3],[1,2,3],[1,2,3]])
     
+    def test_a_loot_with_an_simple_eager_example(self):
+        self.assertEqual(self.s.split([1,3,4],2), [[1,3],[4]])
+        
